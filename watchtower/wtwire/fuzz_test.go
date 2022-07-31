0882a80a6c2afd768e409ecf378181c26587999f
Conner <connerbabs@gmail.com>
2022-07-30 20:24:08 -0400

    Add wtwire diff

--- watchtower/wtwire/fuzz_test.go
+++ watchtower/wtwire/fuzz_test.go
@@ -1,15 +1,17 @@
-package wtwire
+package wtwirefuzz
 
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"reflect"
-	"testing"
+
+	"github.com/lightningnetwork/lnd/watchtower/wtwire"
 )
 
 // prefixWithMsgType takes []byte and adds a wire protocol prefix
 // to make the []byte into an actual message to be used in fuzzing.
-func prefixWithMsgType(data []byte, prefix MessageType) []byte {
+func prefixWithMsgType(data []byte, prefix wtwire.MessageType) []byte {
 	var prefixBytes [2]byte
 	binary.BigEndian.PutUint16(prefixBytes[:], uint16(prefix))
 	data = append(prefixBytes[:], data...)
@@ -21,7 +23,7 @@ func prefixWithMsgType(data []byte, prefix MessageType) []byte {
 // is a valid message once deserialized, and passes a sequence of serialization
 // and deserialization checks. Returns an int that determines whether the input
 // is unique or not.
-func harness(t *testing.T, data []byte, emptyMsg Message) {
+func harness(data []byte, emptyMsg wtwire.Message) int {
 	// Create a reader with the byte array.
 	r := bytes.NewReader(data)
 
@@ -32,152 +34,144 @@ func harness(t *testing.T, data []byte, emptyMsg Message) {
 	payloadLen := uint32(len(data)) - 2
 	if payloadLen > emptyMsg.MaxPayloadLength(0) {
 		// Ignore this input - max payload constraint violated.
-		return
+		return 1
 	}
 
-	msg, err := ReadMessage(r, 0)
+	msg, err := wtwire.ReadMessage(r, 0)
 	if err != nil {
 		// go-fuzz generated []byte that cannot be represented as a
 		// wire message but we will return 0 so go-fuzz can modify the
 		// input.
-		return
+		return 1
 	}
 
 	// We will serialize the message into a new bytes buffer.
 	var b bytes.Buffer
-	if _, err := WriteMessage(&b, msg, 0); err != nil {
+	if _, err := wtwire.WriteMessage(&b, msg, 0); err != nil {
 		// Could not serialize message into bytes buffer, panic.
-		t.Fatal(err)
+		panic(err)
 	}
 
 	// Deserialize the message from the serialized bytes buffer, and then
 	// assert that the original message is equal to the newly deserialized
 	// message.
-	newMsg, err := ReadMessage(&b, 0)
+	newMsg, err := wtwire.ReadMessage(&b, 0)
 	if err != nil {
 		// Could not deserialize message from bytes buffer, panic.
-		t.Fatal(err)
+		panic(err)
 	}
 
 	if !reflect.DeepEqual(msg, newMsg) {
 		// Deserialized message and original message are not
 		// deeply equal.
-		t.Fatal("deserialized message and original message " +
-			"are not deeply equal.")
+		panic(fmt.Errorf("deserialized message and original message " +
+			"are not deeply equal."))
 	}
 
 	// Add this input to the corpus.
-	return
+	return 1
 }
 
-func Fuzz_create_session_reply(f *testing.F) {
-	f.Fuzz(func(t *testing.T, data []byte) {
-		// Prefix with MsgCreateSessionReply.
-		data = prefixWithMsgType(data, MsgCreateSessionReply)
+// Fuzz_create_session_reply is used by go-fuzz.
+func Fuzz_create_session_reply(data []byte) int {
+	// Prefix with MsgCreateSessionReply.
+	data = prefixWithMsgType(data, wtwire.MsgCreateSessionReply)
 
-		// Create an empty message so that the FuzzHarness func can check if the
-		// max payload constraint is violated.
-		emptyMsg := CreateSessionReply{}
+	// Create an empty message so that the FuzzHarness func can check if the
+	// max payload constraint is violated.
+	emptyMsg := wtwire.CreateSessionReply{}
 
-		// Pass the message into our general fuzz harness for wire messages!
-		harness(t, data, &emptyMsg)
-	})
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
 }
 
-func Fuzz_create_session(f *testing.F) {
-	f.Fuzz(func(t *testing.T, data []byte) {
-		// Prefix with MsgCreateSession.
-		data = prefixWithMsgType(data, MsgCreateSession)
+// Fuzz_create_session is used by go-fuzz.
+func Fuzz_create_session(data []byte) int {
+	// Prefix with MsgCreateSession.
+	data = prefixWithMsgType(data, wtwire.MsgCreateSession)
 
-		// Create an empty message so that the FuzzHarness func can check if the
-		// max payload constraint is violated.
-		emptyMsg := CreateSession{}
+	// Create an empty message so that the FuzzHarness func can check if the
+	// max payload constraint is violated.
+	emptyMsg := wtwire.CreateSession{}
 
-		// Pass the message into our general fuzz harness for wire messages!
-		harness(t, data, &emptyMsg)
-	})
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
 }
 
-func Fuzz_delete_session_reply(f *testing.F) {
-	f.Fuzz(func(t *testing.T, data []byte) {
-		// Prefix with MsgDeleteSessionReply.
-		data = prefixWithMsgType(data, MsgDeleteSessionReply)
+// Fuzz_delete_session_reply is used by go-fuzz.
+func Fuzz_delete_session_reply(data []byte) int {
+	// Prefix with MsgDeleteSessionReply.
+	data = prefixWithMsgType(data, wtwire.MsgDeleteSessionReply)
 
-		// Create an empty message so that the FuzzHarness func can check if the
-		// max payload constraint is violated.
-		emptyMsg := DeleteSessionReply{}
+	// Create an empty message so that the FuzzHarness func can check if the
+	// max payload constraint is violated.
+	emptyMsg := wtwire.DeleteSessionReply{}
 
-		// Pass the message into our general fuzz harness for wire messages!
-		harness(t, data, &emptyMsg)
-	})
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
 }
 
-func Fuzz_delete_session(f *testing.F) {
-	f.Fuzz(func(t *testing.T, data []byte) {
-		// Prefix with MsgDeleteSession.
-		data = prefixWithMsgType(data, MsgDeleteSession)
+// Fuzz_delete_session is used by go-fuzz.
+func Fuzz_delete_session(data []byte) int {
+	// Prefix with MsgDeleteSession.
+	data = prefixWithMsgType(data, wtwire.MsgDeleteSession)
 
-		// Create an empty message so that the FuzzHarness func can check if the
-		// max payload constraint is violated.
-		emptyMsg := DeleteSession{}
+	// Create an empty message so that the FuzzHarness func can check if the
+	// max payload constraint is violated.
+	emptyMsg := wtwire.DeleteSession{}
 
-		// Pass the message into our general fuzz harness for wire messages!
-		harness(t, data, &emptyMsg)
-	})
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
 }
 
-func Fuzz_error(f *testing.F) {
-	f.Fuzz(func(t *testing.T, data []byte) {
-		// Prefix with MsgError.
-		data = prefixWithMsgType(data, MsgError)
+// Fuzz_error is used by go-fuzz.
+func Fuzz_error(data []byte) int {
+	// Prefix with MsgError.
+	data = prefixWithMsgType(data, wtwire.MsgError)
 
-		// Create an empty message so that the FuzzHarness func can check if the
-		// max payload constraint is violated.
-		emptyMsg := Error{}
+	// Create an empty message so that the FuzzHarness func can check if the
+	// max payload constraint is violated.
+	emptyMsg := wtwire.Error{}
 
-		// Pass the message into our general fuzz harness for wire messages!
-		harness(t, data, &emptyMsg)
-	})
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
 }
 
-func Fuzz_init(f *testing.F) {
-	f.Fuzz(func(t *testing.T, data []byte) {
-		// Prefix with MsgInit.
-		data = prefixWithMsgType(data, MsgInit)
+// Fuzz_init is used by go-fuzz.
+func Fuzz_init(data []byte) int {
+	// Prefix with MsgInit.
+	data = prefixWithMsgType(data, wtwire.MsgInit)
 
-		// Create an empty message so that the FuzzHarness func can check if the
-		// max payload constraint is violated.
-		emptyMsg := Init{}
+	// Create an empty message so that the FuzzHarness func can check if the
+	// max payload constraint is violated.
+	emptyMsg := wtwire.Init{}
 
-		// Pass the message into our general fuzz harness for wire messages!
-		harness(t, data, &emptyMsg)
-	})
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
 }
 
-func Fuzz_state_update_reply(f *testing.F) {
-	f.Fuzz(func(t *testing.T, data []byte) {
-		// Prefix with MsgStateUpdateReply.
-		data = prefixWithMsgType(data, MsgStateUpdateReply)
+// Fuzz_state_update_reply is used by go-fuzz.
+func Fuzz_state_update_reply(data []byte) int {
+	// Prefix with MsgStateUpdateReply.
+	data = prefixWithMsgType(data, wtwire.MsgStateUpdateReply)
 
-		// Create an empty message so that the FuzzHarness func can check if the
-		// max payload constraint is violated.
-		emptyMsg := StateUpdateReply{}
+	// Create an empty message so that the FuzzHarness func can check if the
+	// max payload constraint is violated.
+	emptyMsg := wtwire.StateUpdateReply{}
 
-		// Pass the message into our general fuzz harness for wire messages!
-		harness(t, data, &emptyMsg)
-	})
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
 }
 
-func Fuzz_state_update(f *testing.F) {
-	f.Fuzz(func(t *testing.T, data []byte) {
-		// Prefix with MsgStateUpdate.
-		data = prefixWithMsgType(data, MsgStateUpdate)
+// Fuzz_state_update is used by go-fuzz.
+func Fuzz_state_update(data []byte) int {
+	// Prefix with MsgStateUpdate.
+	data = prefixWithMsgType(data, wtwire.MsgStateUpdate)
 
-		// Create an empty message so that the FuzzHarness func can check if the
-		// max payload constraint is violated.
-		emptyMsg := StateUpdate{}
+	// Create an empty message so that the FuzzHarness func can check if the
+	// max payload constraint is violated.
+	emptyMsg := wtwire.StateUpdate{}
 
-		// Pass the message into our general fuzz harness for wire messages!
-		harness(t, data, &emptyMsg)
-	})
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
 }
